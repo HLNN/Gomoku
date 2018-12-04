09403a6f724d027e98797e325008d599516cf279
HLN <huangln555@gmail.com>
2018-12-04 09:56:04 +0800

    move Class Game to a individual file

--- Gomoku/Form1.cs
+++ Gomoku/Form1.cs
@@ -13,92 +13,6 @@ using System.Threading;
 
 namespace Gomoku
 {
-    [Serializable]
-    public class Game
-    {
-
-        Graphics graphic;
-
-
-        static readonly DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
-        readonly long gameStartTime = (long)(DateTime.Now - startTime).TotalMilliseconds;
-
-
-        //是否开始
-        public bool Start { get; set; }
-        public bool Review { get; set; }
-        public int Steps { get; set; }
-
-        public bool AllowToMove { get; set; }
-        public bool OldAllowToMove { get; set; }
-        public bool HumanVShuman { get; set; }
-        public bool HumanVSai { get; set; }
-
-        public int WhoToMove { get; set; }
-        public int Winner { get; set; }
-
-        public int BlackTime { get; set; }
-        public int WhiteTime { get; set; }
-        public int BlackTimeThis { get; set; }
-        public int WhiteTimeThis { get; set; }
-
-        // 1 for player 1(black) 2 for player 2(white)
-        public int[,] Chess = new int[15, 15];
-        public int[,] Moves = new int[230, 6];
-
-        public void Move(int x, int y)
-        {
-            Chess[x, y] = WhoToMove;
-            
-            int moveTime = (int)((long)(DateTime.Now - startTime).TotalMilliseconds - gameStartTime);
-            
-            Moves[Steps, 0] = WhoToMove;
-            Moves[Steps, 1] = moveTime;
-            Moves[Steps, 2] = x;
-            Moves[Steps, 3] = y;
-            Moves[Steps, 4] = BlackTime;
-            Moves[Steps, 5] = WhiteTime;
-
-
-            Steps++;
-        }
-
-        public Game()
-        {
-
-            Start = false;
-            Review = false;
-            AllowToMove = false;
-            OldAllowToMove = false;
-            HumanVShuman = false;
-            HumanVSai = false;
-
-            WhoToMove = 1;
-            Winner = -1;
-
-            for (int i = 0; i < 15; i++)
-            {
-                for (int j = 0; j < 15; j++)
-                {
-                    Chess[i, j] = 0;
-                }
-            }
-            for (int i = 0; i < 230; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    Moves[i, j] = 0;
-                }
-            }
-
-            BlackTime = 0;
-            WhiteTime = 0;
-            BlackTimeThis = 0;
-            WhiteTimeThis = 0;
-
-        }
-    }
-
     public partial class Gomoku : Form
     {
         Graphics graphic;
@@ -107,7 +21,6 @@ namespace Gomoku
 
         Game game;
         AI ai;
-
 
         static readonly public Pen pen = new Pen(Color.Black, 1.0f);
         static readonly public Brush whiteBrush = new SolidBrush(Color.FromArgb(255, 255, 255));
@@ -613,5 +526,4 @@ namespace Gomoku
             }
         }
     }
-
 }
---
+++ Gomoku/Game.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku
+{
+    [Serializable]
+    public class Game
+    {
+        static readonly DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+        readonly long gameStartTime = (long)(DateTime.Now - startTime).TotalMilliseconds;
+
+
+        //是否开始
+        public bool Start { get; set; }
+        public bool Review { get; set; }
+        public int Steps { get; set; }
+
+        public bool AllowToMove { get; set; }
+        public bool OldAllowToMove { get; set; }
+        public bool HumanVShuman { get; set; }
+        public bool HumanVSai { get; set; }
+
+        public int WhoToMove { get; set; }
+        public int Winner { get; set; }
+
+        public int BlackTime { get; set; }
+        public int WhiteTime { get; set; }
+        public int BlackTimeThis { get; set; }
+        public int WhiteTimeThis { get; set; }
+
+        // 1 for player 1(black) 2 for player 2(white)
+        public int[,] Chess = new int[15, 15];
+        public int[,] Moves = new int[230, 6];
+
+        public void Move(int x, int y)
+        {
+            Chess[x, y] = WhoToMove;
+
+            int moveTime = (int)((long)(DateTime.Now - startTime).TotalMilliseconds - gameStartTime);
+
+            Moves[Steps, 0] = WhoToMove;
+            Moves[Steps, 1] = moveTime;
+            Moves[Steps, 2] = x;
+            Moves[Steps, 3] = y;
+            Moves[Steps, 4] = BlackTime;
+            Moves[Steps, 5] = WhiteTime;
+
+
+            Steps++;
+        }
+
+        public Game()
+        {
+
+            Start = false;
+            Review = false;
+            AllowToMove = false;
+            OldAllowToMove = false;
+            HumanVShuman = false;
+            HumanVSai = false;
+
+            WhoToMove = 1;
+            Winner = -1;
+
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    Chess[i, j] = 0;
+                }
+            }
+            for (int i = 0; i < 230; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Moves[i, j] = 0;
+                }
+            }
+
+            BlackTime = 0;
+            WhiteTime = 0;
+            BlackTimeThis = 0;
+            WhiteTimeThis = 0;
+        }
+    }
+}
