449e47e79a33bdbfbcd6399c1346213f4af80d78
HLN <huangln555@gmail.com>
2018-12-05 18:37:30 +0800

    add server

---
+++ Server/app.py
@@ -0,0 +1,103 @@
+#! /usr/bin python3
+# -*- coding: utf-8 -*-
+
+import re
+import os
+import random
+import falcon
+
+
+class Board:
+    def __init__(self):
+        self.chess = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+class Live:
+    def __init__(self, b):
+        self.board = b
+
+    def on_get(self, req, resp):
+        # query_string = req.query_string
+        content = ""
+        try:
+            for i in range(15):
+                for j in range(15):
+                    content += str(self.board.chess[i][j]) + " "
+                content += "\n"
+        except:
+            content = "暂无对局"
+
+        resp.body = (content)
+        resp.status = falcon.HTTP_200
+
+class New:
+    def __init__(self, b):
+        self.board = b
+
+    def on_get(self, req, resp):
+        try:
+            self.board.chess = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+            content = "OK"
+        except:
+            content = "WRONG"
+
+        resp.body = (content)
+        resp.status = falcon.HTTP_200
+
+class Move:
+    def __init__(self, b):
+        self.board = b
+
+    def on_get(self, req, resp):
+        query_string = req.query_string
+        query_list = query_string.split('&')
+
+        try:
+            self.board.chess[int(query_list[0])][int(query_list[1])] = int(query_list[2])
+            content = "OK"
+        except:
+            content = "WRONG"
+
+        resp.body = (content)
+        resp.status = falcon.HTTP_200
+
+
+app = application = falcon.API()
+
+board = Board()
+
+live = Live(board)
+app.add_route("/live", live)
+
+new = New(board)
+app.add_route("/new", new)
+
+move = Move(board)
+app.add_route("/move", move)
---
+++ Server/run.sh
@@ -0,0 +1,1 @@
+gunicorn --reload -b 0.0.0.0:80 gomoku.app
